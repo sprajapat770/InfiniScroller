687d8df44dcfbaf83fe8ad037076d533e524ee24
suraj <sprajapat770@gmail.com>
2020-11-06 19:10:46 +0530

    2st Commit -- get dom element height and width in viewModel & use data-bind='afterRender:setContainerElement'

--- view/frontend/web/js/scroller.js
+++ view/frontend/web/js/scroller.js
@@ -1,6 +1,12 @@
 define(['uiComponent','Suraj_InfiniScroll/js/item'],function (Component,ItemComponent){
     "use strict";
 
+    function viewPort(element){
+        return{
+            width: element.clientWidth,
+            height:document.documentElement.clientHeight
+        };
+    }
     return Component.extend({
         defaults:{
             items:[],
@@ -9,11 +15,15 @@ define(['uiComponent','Suraj_InfiniScroll/js/item'],function (Component,ItemComp
             }
         },
         renderItem:function (item){
-            this.items.push(new ItemComponent({item:item}))
+            this.items.push(new ItemComponent({item:item}));
         },
         renderItems:function (items){
-            items.forEach(this.renderItem.bind(this))
-        }
+            items.forEach(this.renderItem.bind(this));
+        },
+        setContainerElement:function(domNode){
+            this.containerElement = domNode;
+        },
+        viewPort
     });
 
 });
