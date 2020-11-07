cec5e73b4fadfcc8a927c7dc726b5ef4ec2caa28
suraj <sprajapat770@gmail.com>
2020-11-07 16:04:55 +0530

    7th commit calculating the currentpage and pagesize for graphql queries

--- view/frontend/web/js/app/graphql-pager-test.js
+++ view/frontend/web/js/app/graphql-pager-test.js
@@ -1,8 +1,32 @@
 define(['Suraj_InfiniScroll/js/test'],function (test){
     "use strict";
 
-    return function(sut){
-        test.assertEquals({a:1},{a:2},{testCase:"example"});
+    return function(getPagingInfo){
+        function assertGetPagingInfoFor(known,toLoad,expected){
+            test.assertEquals(expected,getPagingInfo(known,toLoad),{known,toLoad});        
+        }
+        //degenerate test cases
+        assertGetPagingInfoFor(0,-1,{pageSize:0,currentPage:1});
+        assertGetPagingInfoFor(-1,1,{pageSize:0,currentPage:1});
+        assertGetPagingInfoFor(0,0,{pageSize:0,currentPage:1});
+
+        //zero known products
+        assertGetPagingInfoFor(0,1,{pageSize:1,currentPage:1});
+        assertGetPagingInfoFor(0,5,{pageSize:5,currentPage:1});
+
+        // known is evenly divideable by toLoad
+        assertGetPagingInfoFor(3,3,{pageSize:3,currentPage:2});
+        assertGetPagingInfoFor(10,5,{pageSize:5,currentPage:3});
+        assertGetPagingInfoFor(6,1,{pageSize:1,currentPage:7});
+
+        // known is NOT evenly divideable by toLoad
+        assertGetPagingInfoFor(3,2,{pageSize:3,currentPage:2});
+        assertGetPagingInfoFor(6,4,{pageSize:6,currentPage:2});
+        assertGetPagingInfoFor(20,6,{pageSize:10,currentPage:3});
+        assertGetPagingInfoFor(11,3,{pageSize:11,currentPage:2});
+        //zero < known < toLoad
+        //assertGetPagingInfoFor(10,14,{pageSize:24,currentPage:1});
+        //assertGetPagingInfoFor(5,7,{pageSize:12,currentPage:1});
     }
 
 });
--- view/frontend/web/js/app/graphql-pager.js
+++ view/frontend/web/js/app/graphql-pager.js
@@ -1,8 +1,22 @@
 define([],function (){
     "use strict";
 
-    return function(){
-  
+
+    function findNextDivisor(known,toLoad){
+    	if (known % toLoad === 0) return toLoad;
+    	return findNextDivisor(known,toLoad+1);
+    }
+
+    return function(known,toLoad){
+    	if (known < 0 || toLoad <= 0) {
+  		return {pageSize:0,currentPage:1};
+
+    	}
+    	if ( toLoad >known) {
+    		return {pageSize: known+toLoad,currentPage:1};
+    	}
+    	const pageSize = findNextDivisor(known,toLoad);
+    	return {pageSize:pageSize,currentPage:(known/pageSize)+1};
     }
 
 });
